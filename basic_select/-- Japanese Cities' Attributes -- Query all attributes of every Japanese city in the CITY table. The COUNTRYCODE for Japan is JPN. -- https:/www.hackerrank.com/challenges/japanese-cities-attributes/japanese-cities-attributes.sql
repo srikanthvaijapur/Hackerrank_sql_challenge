9d002f0d1969db35f145adbfb12a8af1900d354a
Srikanth Vaijapur <49674486+srikanthvaijapur@users.noreply.github.com>
2019-12-24 13:33:31 +0530

    Delete japanese-cities-attributes.sql

--- basic_select/-- Japanese Cities' Attributes -- Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN. -- https:/www.hackerrank.com/challenges/japanese-cities-attributes/japanese-cities-attributes.sql
+++
@@ -1,7 +0,0 @@
--- Japanese Cities' Attributes
--- Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
--- https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
-
-SELECT *
-FROM City
-WHERE CountryCode = 'JPN';
